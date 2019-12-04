e9ee83ad283449eadac22a18f244851f7b70bbf6
Pavel <1879936@gmail.com>
2019-12-04 23:15:42 +0300

    Homework_6

---
+++ Homework_6/AutoJavaProject/src/test/java/PVTcourses/AppTest.java
@@ -0,0 +1,25 @@
+package PVTcourses;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testGetSumNumbers() {
+        App app = new App();
+        int actual = app.getSumNumbers(1, 2);
+        Assert.assertEquals(actual, 3);
+    }
+}
